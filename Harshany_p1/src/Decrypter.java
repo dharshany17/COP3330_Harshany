d7973e8df397492dfae065c4dbbf7e2e6640a8c3
Dominick Harshany <dominick@harshanytsi.com>
2020-09-14 19:27:48 -0400

    Final Commit

--- Harshany_p1/src/Decrypter.java
+++ Harshany_p1/src/Decrypter.java
@@ -1,2 +1,42 @@
 public class Decrypter {
+    public static String decrypt(String dCode){
+
+        char e=dCode.charAt(0);
+        char f=dCode.charAt(1);
+        char g=dCode.charAt(2);
+        char h=dCode.charAt(3);
+
+        int e2=Character.getNumericValue(e);
+        int f2=Character.getNumericValue(f);
+        int g2=Character.getNumericValue(g);
+        int h2=Character.getNumericValue(h);
+
+        int e3=((e2+10)-7)%10;
+        int f3=((f2+10)-7)%10;
+        int g3=((g2+10)-7)%10;
+        int h3=((h2+10)-7)%10;
+
+        int temp;
+        temp = e3;
+        e3 = g3;
+        g3 = temp;
+
+        int temp2;
+        temp2 = f3;
+        f3 = h3;
+        h3 = temp2;
+
+        String s1 = Integer.toString(e3);
+        String s2 = Integer.toString(f3);
+        String s3 = Integer.toString(g3);
+        String s4 = Integer.toString(h3);
+
+        String str1 = s1 + s2 + s3 +s4;
+        System.out.printf("Decrypted code is %s", str1);
+
+
+        return str1;
+    }
+
 }
+
--- Harshany_p1/src/Encrypter.java
+++ Harshany_p1/src/Encrypter.java
@@ -1,5 +1,44 @@
 public class Encrypter {
 
-    BEANS
+    public static String encrypt(String eCode){
+
+        char a=eCode.charAt(0);
+        char b=eCode.charAt(1);
+        char c=eCode.charAt(2);
+        char d=eCode.charAt(3);
+
+        int a2=Character.getNumericValue(a);
+        int b2=Character.getNumericValue(b);
+        int c2=Character.getNumericValue(c);
+        int d2=Character.getNumericValue(d);
+
+        int a3=(a2+7)%10;
+        int b3=(b2+7)%10;
+        int c3=(c2+7)%10;
+        int d3=(d2+7)%10;
+
+
+        int temp;
+        temp = a3;
+        a3 = c3;
+        c3 = temp;
+
+        int temp2;
+        temp2 = b3;
+        b3 = d3;
+        d3 = temp2;
+
+        String s1 = Integer.toString(a3);
+        String s2 = Integer.toString(b3);
+        String s3 = Integer.toString(c3);
+        String s4 = Integer.toString(d3);
+
+        String str1 = s1 + s2 + s3 +s4;
+        System.out.printf("Encrypted code is %s", str1);
+
+
+        return str1;
+    }
+
 
 }
